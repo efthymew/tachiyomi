871d2151c2c840b2377c53b483559eaf8fe98158
graham <grahomie@gmail.com>
2021-12-21 17:57:37 -0500

    latest stuff

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderTranslateSheet.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderTranslateSheet.kt
@@ -1,13 +1,19 @@
 package eu.kanade.tachiyomi.ui.reader
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
+import com.googlecode.tesseract.android.TessBaseAPI
 import eu.kanade.tachiyomi.databinding.ReaderTranslateSheetBinding
 import eu.kanade.tachiyomi.ui.reader.model.ReaderPage
 import eu.kanade.tachiyomi.widget.sheet.BaseBottomSheetDialog
-import com.googlecode.tesseract.android.TessBaseAPI
-
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.lang.Exception
 
 class ReaderTranslateSheet(
     private val activity: ReaderActivity,
@@ -25,7 +31,64 @@ class ReaderTranslateSheet(
 
     fun translate() {
         val currPage = page ?: return
-        val url = currPage.imageUrl
+        val streamFn = currPage.stream ?: return
+        val fileStream: InputStream = streamFn()
+        prepareOCR()
         val tessBaseApi = TessBaseAPI()
+        val text = getText(fileStream, tessBaseApi)
+        binding.translation.setText(text)
+    }
+
+    private fun getText(fileStream: InputStream, api: TessBaseAPI): String? {
+        val bitmap: Bitmap = BitmapFactory.decodeStream(fileStream)
+        api.init(DATA_PATH, "jpn")
+        api.setImage(bitmap)
+        var text = ""
+        try {
+            // call api
+        } catch (e: Exception) {
+        }
+        return text
+    }
+
+    private fun prepareDir(path: String) {
+        val dir = File(path)
+        if (!dir.exists()) {
+            if (!dir.mkdirs()) {
+                // nothing
+            }
+        }
+    }
+
+    private fun prepareOCR() {
+        try {
+            prepareDir(DATA_PATH + TESSDATA)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        copyOCRModel(TESSDATA)
+    }
+
+    private fun copyOCRModel(path: String) {
+        try {
+            val files = activity.assets.list(path) ?: return
+            for (file in files) {
+                val dataPath = "$DATA_PATH$path/$file"
+                if (!File(dataPath).exists()) {
+                    val inp: InputStream = activity.assets.open("$path/$file")
+                    val out: OutputStream = FileOutputStream(dataPath)
+                    val buf = ByteArray(1024)
+                    var len = inp.read(buf)
+                    while (len > 0) {
+                        out.write(buf, 0, len)
+                        len = inp.read(buf)
+                    }
+                    inp.close()
+                    out.close()
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
